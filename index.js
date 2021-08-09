56d35a326694416cf449c04ede2b2c7876359864
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-09 13:00:21 -0400

    commit tree 9710ea586da7692f1ef207fa85280c50442f2e35

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random() *10) +1;
+
+let mod;
+let num3 = 24;
+let num4 = 10;
+mod = num3 % num4;
+
+let max = Math.max(15,6,14,20);
 
 // Write your code here
